f826cbb2d75be139dc1a0594b783932ae5fff610
Prachi Gore <prachigore408@gmail.com>
2023-05-12 23:09:19 +0530

    Update PCard.jsx

--- src/Components/PCard.jsx
+++ src/Components/PCard.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import mmt from '../assets/mmt.mp4'
 
 function PCard() {
   return (
     <Container>
-     <video></video>
-     <h4>Model Monitoring Tool</h4>
+    <div className='video'>
+    <video   controls src={mmt} type="video/mp4"></video>
+    
+     <h2>Model Monitoring Tool</h2>
+     </div>
 <SubContainer>
     <p className='descr'></p>
     <Button><i class="fa-regular fa-eye"></i>Preview</Button>
@@ -18,9 +22,42 @@ function PCard() {
 export default PCard;
 
 const Container=styled.div`
+
+
+.video{
+ 
+  
+    
+    
+  
+  video{
+   height:300px;
+    width:100%;
+    cursor: pointer;
+    margin-bottom: -25px;
+    ::-webkit-media-controls-panel{
+      display: none !important;
+    }
+  
+  }
+
+
+  h2{
+   height: 50px;
+   background-color: #ffbf00;
+   color: black;
+   display: flex;
+   align-items: center;
+   padding-left:10px;
+   margin-top:0px;
+
+  }
+  
+}
     
 `;
 const SubContainer=styled.div`
+display: none;
     
 `;
 const Button=styled.button`

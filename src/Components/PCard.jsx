49bd78bf8f579afa35c68c36c6aeefd6753f4716
Prachi Gore <prachigore408@gmail.com>
2023-06-04 08:58:47 +0530

    Update PCard.jsx

--- src/Components/PCard.jsx
+++ src/Components/PCard.jsx
@@ -10,7 +10,7 @@ function PCard({src,name,descr,code,preview}) {
    
     <Container>
    
-    <video    controls  src={'../src/assets/finance_calculator.mp4'} type="video/mp4"></video>
+    <video    controls  src={'../dist/assets/finance_calculator.mp4'} type="video/mp4"></video>
     
      <h2>{name}</h2>
      <SubContainer className='subcontainer'>

e2b87ecf9f825a781e336dbbb9dee054096ea0b4
Prachi Gore <prachigore408@gmail.com>
2023-06-04 12:21:41 +0530

    Update Project.jsx

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
 import PCard from './PCard'
-// import data from './project.json';
+//import data from './project.json'
 import finance_calculator from '../assets/finance_calculator.mp4';
 import blog_shiny from '../assets/blog_shiny.mp4';
 import amazon_frontend from '../assets/amazon_frontend.mp4';
 import netflix_frontend from '../assets/netflix_frontend.mp4';
+import dvmft from '../assets/dvmft.mp4';
+import fun_with_triangle from '../assets/fun_with_triangle.mp4';
+import airbnb from '../assets/airbnb.mp4';
+import react_info_app from '../assets/react_info_app.mp4';
+import random_multiplication from '../assets/random_multiplication.mp4';
+import cash_register_manager from '../assets/cash_register_manager.mp4';
+import char_count from '../assets/char_count.mp4';
+import digital_clock from '../assets/digital_clock.mp4';
+import stock_price from '../assets/stock_price.mp4';
+import quiz_cli from '../assets/quiz_cli.mp4';
+import palindrome_birthday from '../assets/palindrome_birthday.mp4';
+import meme_generator from '../assets/meme_generator.mp4';
+import lucky_bday from '../assets/lucky_bday.mp4';
 
+ //const arraySrc=[amazon_frontend,finance_calculator,netflix_frontend,dvmft,fun_with_triangle,airbnb,meme_generator,react_info_app,random_multiplication,cash_register_manager,char_count,digital_clock,stock_price,quiz_cli,palindrome_birthday,blog_shiny,lucky_bday]
 
 const data=[
   {
@@ -24,7 +38,7 @@ const data=[
   "preview":"https://prachigore29.shinyapps.io/Finance_Calculator/"
 },
 {
-  "src":"../assets/dvmft.mp4",
+  "src":dvmft,
   "name":"Data Visualization&Model Fitting Tool",
   "descr":"Now user can visualize dataset, fit the model without knowing any programming language",
   "code":"https://github.com/Prachi-Gore/Model-Fitting-Data-Visualization-Tool",
@@ -39,49 +53,49 @@ const data=[
   "preview":"https://netflix-ui-react-scss.netlify.app/"
 },
 {
-  "src":"../src/assets/fun_with_triangle.mp4",
+  "src":fun_with_triangle,
   "name":"Fun With Triangle📐",
   "descr":"This is multi page website Here you can check whether given geometry is triangle? Area? Hypotenous? Quizzes ",
   "code":"https://github.com/Prachi-Gore/Fun_With_Triangles",
   "preview":"https://learn-triangles-with-fun.netlify.app/"
 },
 {
-  "src":"../src/assets/airbnb.mp4",
+  "src":airbnb,
   "name":"Airbnb",
   "descr":"This is Airbnb clone build with HTML, CSS, ReactJS",
   "code":"https://github.com/Prachi-Gore/airbnb-clone",
   "preview":"https://airbnbscrimba.netlify.app/"
 },
 {
-  "src":"../src/assets/react_info_app.mp4",
+  "src":react_info_app,
   "name":"React Info App",
   "descr":"Do you want to know about react ",
   "code":"https://github.com/Prachi-Gore/react-info-app",
   "preview":"https://react-info-app.netlify.app/"
 },
 {
-  "src":"../src/assets/random_multiplication.mp4",
+  "src":random_multiplication,
   "name":"Multiplication🧮 App",
   "descr":"Practice multiplication and keep track of score",
   "code":"https://github.com/Prachi-Gore/MultiplicationApp",
   "preview":"https://multiplicationpractice.netlify.app/"
 },
 {
-  "src":"../src/assets/cash_register_manager.mp4",
+  "src":cash_register_manager,
   "name":"Cash💵 Register Manager",
   "descr":"help the user by telling how can he/she return the change to the customer with a minimum number of notes ",
   "code":"https://github.com/Prachi-Gore/Cash-Register-Manager",
   "preview":"https://cash-exchange-guide.netlify.app/"
 },
 {
-"src":"../src/assets/char_count.mp4",
+"src":char_count,
 "name":"Count Character",
 "descr":"let's count characters",
 "code":"https://github.com/Prachi-Gore/Count_Characters",
 "preview":"https://prachi-gore.github.io/Count_Characters/"
 },
 {
-"src":"../src/assets/digital_clock.mp4",
+"src":digital_clock,
 "name":"Digital Clock⏰",
 "descr":"This is Digital Clock",
 "code":"https://github.com/Prachi-Gore/DigitalClock",
@@ -89,35 +103,35 @@ const data=[
 
 },
 {
-"src":"../src/assets/lucky_bday.mp4",
+"src":lucky_bday,
 "name":"Is Your 🎂 Lucky",
 "descr":"This is fun app to see whether your Birthdate is Lucky",
 "code":"https://github.com/Prachi-Gore/isYourBirthdayLucky",
 "preview":"https://is-your-birthdate-special.netlify.app/"
 },
 {
-"src":"../src/assets/meme_generator.mp4",
+"src":meme_generator,
 "name":"meme 🎭 Generator",
 "descr":"This is meme generator",
 "code":"https://github.com/Prachi-Gore/meme-generator",
 "preview":"https://generate-funny-meme.netlify.app/"
 },
 {
-"src":"../src/assets/palindrome_birthday.mp4",
+"src":palindrome_birthday,
 "name":"Palindrome Birthday🍰",
 "descr":"palindrome birthday checker",
 "code":"https://github.com/Prachi-Gore/palindrome-birthday",
 "preview":"https://palindromric-birthday.netlify.app/"
 },
 {
-"src":"../src/assets/quiz_cli.mp4",
+"src":quiz_cli,
 "name":"Quiz🤔 App",
 "descr":"let's play, how well do you know india . this APP made on Command Line Interface",
 "code":"https://github.com/Prachi-Gore/Quiz_in_CLI",
 "preview":"https://replit.com/@Prachi-Gore/Quiz#index.js?embed=1&output=1"
 },
 {
-"src":"../src/assets/stock_price.mp4",
+"src":stock_price,
 "name":"Stock Price💰 Calculator",
 "descr":"This is Stock Purchase Profit and Loss Calculator App ",
 "code":"https://github.com/Prachi-Gore/Stock-Price-Calculator",
@@ -146,8 +160,11 @@ function Project() {
     <h1>Projects <span style={{color:"#ffff00"}}>Made</span></h1>
     </p>
     <ProjectContainer>
-     { data.map((item)=><PCard src={item.src} name={item.name} descr={item.descr} preview={item.preview} code={item.code}></PCard>)}
-    
+     {data.map((item)=><PCard src={item.src} name={item.name} descr={item.descr} preview={item.preview} code={item.code}></PCard>)}
+    {/* dont do this */}
+     {/* {arraySrc.map((a)=>data.map((item)=><PCard src={a} name={item.name} descr={item.descr} preview={item.preview} code={item.code}></PCard>))} */}
+     
+     
     </ProjectContainer>
     </Container>
      </section >
